300a16ed43637d14ee953c78b534a79b70a9965f
Chrsitopher de Beer <christopherdebeer@gmail.com>
2011-05-30 21:43:17 +0100

    changes :)

--- colourtool.js
+++ colourtool.js
@@ -2,9 +2,9 @@ var colourtool = {
     init: function () {
         if (window.console) {console.log("ColourTool init...");}
         $("body").html("<div id='colourtool'><h1>ColourTool</h1></div>");
-        $("link[type='text/css']").each( function(stylesheet){
-            console.log(stylesheet);
-            $("body").append("<p>link found.</p>");
+        $("link[type='text/css']").each( function(index,stylesheet){
+            if (window.console) {console.log(stylesheet.attr("href"));}
+            $("#colourtool").append("<p>link found.</p>");
         });
     }
 }
