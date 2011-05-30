bf289635c90419a10ac0687631ff84e94723ae12
Chrsitopher de Beer <christopherdebeer@gmail.com>
2011-05-30 21:20:28 +0100

    check for stylesheets

--- colourtool.js
+++ colourtool.js
@@ -2,7 +2,7 @@ var colourtool = {
     init: function () {
         if (window.console) {console.log("ColourTool init...");}
         $("body").html("<div id='colourtool'><h1>ColourTool</h1></div>");
-        $("link[type=''text/css']").each( function(stylesheet){
+        $("link[type='text/css']").each( function(stylesheet){
             console.log(stylesheet);
             $("body").append("<p>link found.</p>");
         });
