277e238c61703a24156971c7d9b4e14bd4b72b73
Christopher de Beer <christopherdebeer@gmail.com>
2011-05-31 11:16:14 +0100

    added check for style tags

--- colourtool.js
+++ colourtool.js
@@ -3,6 +3,11 @@ var colourtool = {
     allrules: [],
     colours: [],
     fonts: [],
+    regexPatterns: {
+        hex: /#([0-9abcdef]+?){3,6}/i,
+        rgb: "",
+        rgba: ""
+    },
     init: function () {
         if (window.console) {console.log("Init colourTool...")}
         colourtool.getStylesheets()
@@ -13,17 +18,29 @@ var colourtool = {
         // get all stylesheets that dont belong to colourtool
         $(document.styleSheets).each( function(index, stylesheet) {
             if (window.console) {console.log("found stylesheet : " + stylesheet.href)}
-            if (stylesheet.href != "https://github.com/christopherdebeer/colourtool/raw/master/colourtool.css") {colourtool.stylesheets.push(stylesheet)}
+            if (stylesheet.href != "https://github.com/christopherdebeer/colourtool/raw/master/colourtool.css") {
+                colourtool.stylesheets.push(stylesheet)
+            }
         })
         // get all the css rules from the stylesheets
-        $(colourtool.stylesheets).each(function (i,s){
-            $(colourtool.stylesheets[i].cssRules).each(function(x,r){
+        $(colourtool.stylesheets).each( function (i,s){
+            $(colourtool.stylesheets[i].cssRules).each( function(x,r){
                 //if (window.console) {console.log(colourtool.stylesheets[i].cssRules[x].cssText)}
                 colourtool.allrules.push(colourtool.stylesheets[i].cssRules[x].cssText)
             })
         })
     },
     getColours: function () {
-        // search for colours
-    }   
+        
+        // search allrules for colours
+        $(colourtool.allrules).each( function(i,r){
+            if (r.search(colourtool.regexPatterns.hex) != -1) {
+                if (window.console) {console.log("Found a colour")}
+            }
+        })
+    },
+    getFonts: function () {
+        
+        // search allrules for fonts
+    }
 }
