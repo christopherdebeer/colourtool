acd65217f028b9fbb0a945a87d311a95e20ef951
Christopher de Beer <christopherdebeer@gmail.com>
2011-05-31 10:58:44 +0100

    added check for style tags

--- colourtool.js
+++ colourtool.js
@@ -9,21 +9,21 @@ var colourtool = {
         colourtool.getColours()
     },
     getStylesheets: function () {
-        $(document.styleSheets).each( function(i, stylesheet) {
+        
+        // get all stylesheets that dont belong to colourtool
+        $(document.styleSheets).each( function(index, stylesheet) {
             if (window.console) {console.log("found stylesheet : " + stylesheet.href)}
-            if (stylesheet.href != "https://github.com/christopherdebeer/colourtool/raw/master/colourtool.css") {
-                
-                // get all stylesheets and rules
-                colourtool.stylesheets.push(stylesheet)
-                $(colourtool.stylesheets[i].cssRules).each(function(x,r){
-                    if (window.console) {console.log(colourtool.stylesheets[i].cssRules[x].cssText)}
-                    colourtool.allrules.push(colourtool.stylesheets[i].cssRules[x].cssText)
-                })
-            }            
+            if (stylesheet.href != "https://github.com/christopherdebeer/colourtool/raw/master/colourtool.css") {colourtool.stylesheets.push(stylesheet)}
+        })
+        // get all the css rules from the stylesheets
+        $(colourtool.stylesheets).each(function (i,s){
+            $(colourtool.stylesheets[i].cssRules).each(function(x,r){
+                if (window.console) {console.log(colourtool.stylesheets[i].cssRules[x].cssText)}
+                colourtool.allrules.push(colourtool.stylesheets[i].cssRules[x].cssText)
+            })
         })
     },
     getColours: function () {
-        
         // search for colours
     }   
 }
