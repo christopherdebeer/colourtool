173ff426dc39c10d484c02f62bb7fd15d0ec7f27
Christopher de Beer <christopherdebeer@gmail.com>
2011-05-31 10:19:32 +0100

    added check for style tags

--- colourtool.js
+++ colourtool.js
@@ -4,15 +4,21 @@ var colourtool = {
     colours: [],
     fonts: [],
     init: function () {
-        if (window.console) {console.log("ColourTool init...")}
-        $("body").html("<div id='colourtool'><h1>ColourTool</h1></div>")
-        $("link[type='text/css'], link[rel='stylesheet']").each( function(index, stylesheet){
-            if (window.console) {console.log("found linked stylesheet : " + stylesheet.baseURI)}
-            colourtool.stylesheets.push(stylesheet.baseURI)
-        })
-        $("style").each( function(index, styletag){
-            if (window.console) {console.log("found style element : " + styletag)}
-            colourtool.styles.push(styletag.innerHTML)
+        //if (window.console) {console.log("ColourTool init...")}
+        //$("body").html("<div id='colourtool'><h1>ColourTool</h1></div>")
+        //$("link[type='text/css'], link[rel='stylesheet']").each( function(index, stylesheet){
+        //    if (window.console) {console.log("found linked stylesheet : " + stylesheet.baseURI)}
+        //    colourtool.stylesheets.push(stylesheet.baseURI)
+        //})
+        //$("style").each( function(index, styletag){
+        //    if (window.console) {console.log("found style element : " + styletag)}
+        //    colourtool.styles.push(styletag.innerHTML)
+        //})
+        colourtool.getStylesheets()
+    },
+    getStylesheets: function () {
+        $(document.styleSheets).each( function(index, stylesheet) {
+            if (window.console) {console.log("found stylesheet : " + stylesheet.href)}
         })
     }
 }
