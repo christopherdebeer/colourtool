c974c2ac2b843a9d8e5610800ba4f1ddb02127fa
Christopher de Beer <christopherdebeer@gmail.com>
2011-05-31 10:04:07 +0100

    added check for style tags

--- colourtool.js
+++ colourtool.js
@@ -7,8 +7,8 @@ var colourtool = {
         if (window.console) {console.log("ColourTool init...")}
         $("body").html("<div id='colourtool'><h1>ColourTool</h1></div>")
         $("link[type='text/css']").each( function(index, stylesheet){
-            if (window.console) {console.log("found linked stylesheet : " + stylesheet)}
-            colourtool.stylesheets.push(stylesheet)
+            if (window.console) {console.log("found linked stylesheet : " + stylesheet.baseURI)}
+            colourtool.stylesheets.push(stylesheet.baseURI)
         })
         $("style").each( function(index, styletag){
             if (window.console) {console.log("found style element : " + styletag)}
