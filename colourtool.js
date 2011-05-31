5848cf04e3dfbe706a0d9c935b683611f8444388
Christopher de Beer <christopherdebeer@gmail.com>
2011-05-31 18:12:23 +0100

    added check for style tags

--- colourtool.js
+++ colourtool.js
@@ -85,14 +85,14 @@ var colourtool = {
             // #hex
             if (r.search(colourtool.regexPatterns.hex) != -1) {
                 var matches = r.match(colourtool.regexPatterns.hex)
-                $(matches).each( function(index,colour) {colourtool.colours.push(colour.replace(" ",""))})
+                $(matches).each( function(index,colour) {colourtool.colours.push(colour.replace(" ","").toLowerCase())})
                 //if (window.console) {console.log("Found " + matches.length.toString() + " hex colour(s): " + matches)}
             }
             
             // rgb()
             if (r.search(colourtool.regexPatterns.rgb) != -1) {
                 var matches = r.match(colourtool.regexPatterns.rgb)
-                $(matches).each( function(index,colour) {colourtool.colours.push(colour.replace(" ",""))})
+                $(matches).each( function(index,colour) {colourtool.colours.push(colour.replace(" ","").toLowerCase())})
                 //if (window.console) {console.log("Found " + matches.length.toString() + " rgb colour(s): " + matches)}
             }
         })
