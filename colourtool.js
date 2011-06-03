8dd136c69f54a4797942939ec7493763f75cab89
Christopher de Beer <christopherdebeer@gmail.com>
2011-06-03 12:03:55 +0100

    changed css check to use colBase

--- colourtool.js
+++ colourtool.js
@@ -82,6 +82,7 @@ var colourtool = {
         
     },
     yqlLoad: function (url) {
+        if (window.console) {console.log("trying YQL proxy for" + url)}
         var yqlquery = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22"+url+"%22&format=json&callback=?"
         $.getJSON({
             url: yqlquery,
